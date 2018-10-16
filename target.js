8995791a4a97063160e48aff8bd5dc87ddb24872
Mikel Maron <mikel_maron@yahoo.com>
2018-10-16 13:12:45 -0400

    kangemi schools

--- target.js
+++ target.js
@@ -42,6 +42,9 @@
     validate_allowed_list: function(key, value, options) {
       return $.inArray(value,options["allowed_list"]) != -1;
     },
+    validate_yes_no: function(key, value, options) {
+      return $.inArray(value,["yes","no"]) != -1;
+    },
     validate_multi_allowed_list: function(key, value, options) {
       var test = true;
       if (typeof value == "undefined") {
@@ -49,17 +52,21 @@
       }
       var values = value.split(',');
       $.each( values, function( index, v) {
-        if ($.inArray(value,options["allowed_list"]) == -1) {
+        if ($.inArray(v,options["allowed_list"]) == -1) {
           test = false;
         }
       });
-      return true;
+      return test;
     },
     validate_date_after: function(key, value, options) {
       return value > options['date_after'];
     },
     validate_address: function(key, value, options) {
       return false;
+    },
+    validate_url: function(key, value, options) {
+      var pattern = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/
+      return pattern.test(value);
     },
 
     validate_unallowed: function(key, value, options, properties, validate_keys) {
@@ -152,14 +159,14 @@
             //Validations
             var result = true;
             $.each( params['validation'], function(j, validation_callback) {
-              result = result && (target[ validation_callback ])( key, feature['properties']['tags'][key], params['options'], feature['properties']['tags'], Object.keys(target.validate) );
+              result = result && (target[ validation_callback ])( key, feature['properties'][key], params['options'], feature['properties'], Object.keys(target.validate) );
             });
             var result_class = (result ? "valid" : "invalid");
 
             //Filters
-            var filtered_item = (feature['properties']['tags'][key] !== undefined ? feature['properties']['tags'][key] : "");
+            var filtered_item = (feature['properties'][key] !== undefined ? feature['properties'][key] : "");
             if (params['filter'] !== undefined) {
-              filtered_item = target[ params['filter'][0] ]( key, feature['properties']['tags'][key], feature['properties']['tags'], Object.keys(target.validate)  );
+              filtered_item = target[ params['filter'][0] ]( key, feature['properties'][key], feature['properties'], Object.keys(target.validate)  );
             }
 
             item += "<td class='" + result_class + "'>" + filtered_item + "</td>";
@@ -183,18 +190,326 @@ $( function() {
   target.global();
   target.set_validation(
     {
-      "name" : {
+      "osm:name" : {
         "validation" : ["validate_present", "validate_capitalization"]
       },
-      "wb_pb:id" : {
-        "validation" : ["validate_present"]
+      "osm:id": {
+        "validation" : ["validate_present"],
+        "group" : "basic",
+        "filter" : ["filter_id"]
       },
-      "image": {
-        "validation" : ["validate_present"]
+      "osm:_user": {
+        "validation" : ["validate_present"],
+        "group" : "basic"
+      },
+      "osm:_timestamp": {
+        "validation" : ["validate_present"],
+        "group" : "basic"
+      },
+      "osm:accessibility" : {
+        "validation" : ["validate_multi_allowed_list"],
+        "options" : {"allowed_list" : ["big_vehicles","foot","small_cars"]},
+        "group" : "infrastructure"
+      },
+
+      "osm:amenity" : {
+        "validation" : ["validate_multi_allowed_list"],
+        "options" : {"allowed_list" : ["college", "school", "university"]},
+        "group" : "basic"
+      },
+      "osm:building:classrooms" : {
+        "validation" : ["validate_numeric"],
+        "group" : "infrastructure"
+      },
+      "osm:building:levels" : {
+        "validation" : ["validate_numeric"],
+        "group" : "infrastructure"
+      },
+      "osm:building:material" : {
+        "validation" : ["validate_multi_allowed_list"],
+        "options" : {"allowed_list" : ["cement_block","concrete","iron_sheet","wood"]},
+        "group" : "infrastructure"
+      },
+      "osm:building:roof" : {
+        "validation" : ["validate_multi_allowed_list"],
+        "options" : {"allowed_list" : ["asbestos","concrete","iron_sheet"]},
+        "group" : "infrastructure"
+      },
+      "osm:contact:address" : {
+        "validation" : ["validate_present"],
+        "group" : "infrastructure"
+      },
+      "osm:contact:email" : {
+        "validation" : ["validate_present"],
+        "group" : "infrastructure"
+      },
+      "osm:contact:phone" : {
+        "validation" : ["validate_present"],
+        "group" : "infrastructure"
+      },
+      "osm:education:boarding" : {
+        "validation" : ["validate_yes_no"],
+        "group" : "services"
+      },
+      "osm:education:bom_employed" : {
+        "validation" : ["validate_yes_no"],
+        "group" : "population"
+      },
+      "osm:education:bom_how_many" : {
+        "validation" : ["validate_numeric"],
+        "group" : "population"
+      },
+      "osm:education:curriculum" : {
+        "validation": ["validate_allowed_list"],
+        "options" : {"allowed_list" : ["2_6_3_3_3","8_4_4","gcse","montessori","ntsa"]},
+        "group" : "services"
+      },
+      "osm:education:exam_affiliate" : {
+        "validation": [],
+        "group" : "services"
+      },
+      "osm:education:exam_center" : {
+        "validation": ["validate_yes_no"],
+        "group" : "services"
+      },
+      "osm:education:extra_drama" : {
+        "validation": ["validate_yes_no"],
+        "group" : "services"
+      },
+      "osm:education:extra_music" : {
+        "validation": ["validate_yes_no"],
+        "group" : "services"
+      },
+      "osm:education:extra_other" : {
+        "validation": [],
+        "group" : "services"
+      },
+      "osm:education:extra_scouts" : {
+        "validation": ["validate_yes_no"],
+        "group" : "services"
+      },
+      "osm:education:extra_sports" : {
+        "validation": ["validate_yes_no"],
+        "group" : "services"
+      },
+      "osm:education:fees" : {
+        "validation": ["validate_allowed_list"],
+        "options" : {"allowed_list" : ["yes", "sponsored", "free_education"]},
+        "group" : "fees"
+      },
+      "osm:education:fees_level1" : {
+        "validation" : ["validate_numeric"],
+        "group" : "fees"
+      },
+      "osm:education:fees_level2" : {
+        "validation" : ["validate_numeric"],
+        "group" : "fees"
+      },
+      "osm:education:fees_level3" : {
+        "validation" : ["validate_numeric"],
+        "group" : "fees"
+      },
+      "osm:education:fees_per_year" : {
+        "validation" : ["validate_numeric"],
+        "group" : "fees"
+      },
+      "osm:education:fees_admission" : {
+        "validation" : ["validate_numeric"],
+        "group" : "fees"
+      },
+      "osm:education:fees_exam" : {
+        "validation" : ["validate_numeric"],
+        "group" : "fees"
+      },
+      "osm:education:fees_lunch" : {
+        "validation" : ["validate_numeric"],
+        "group" : "fees"
+      },
+      "osm:education:fees_other" : {
+        "validation" : ["validate_numeric"],
+        "group" : "fees"
+      },
+      "osm:education:fees_trip" : {
+        "validation" : ["validate_numeric"],
+        "group" : "fees"
+      },
+      "osm:education:gender_type" : {
+        "validation" : ["validate_allowed_list"],
+        "options": {"allowed_list": ["boys","girls","mixed"]},
+        "group": "basic"
+      },
+      "osm:education:government_grants" : {
+        "validation": ["validate_yes_no"],
+        "group": "basic"
+      },
+      "osm:education:government_registered" : {
+        "validation": ["validate_allowed_list"],
+        "options": {"allowed_list": ["APBET","mgcsd","moe","private","no"]},
+        "group": "basic"
+      },
+      "osm:education:management_committee": {
+        "validation": ["validate_yes_no"],
+        "group": "basic"
+      },
+      "osm:education:mean_score": {
+        "validation": ["validate_numeric"],
+        "group": "basic"
+      },
+      "osm:education:nemis_code": {
+        "validation": ["validate_yes_no"],
+        "group": "basic"
+      },
+      "osm:education:ntsa_approved_teacher": {
+        "validation": ["validate_yes_no"],
+        "group": "population"
+      },
+      "osm:education:ntsa_how_many": {
+        "validation": ["validate_numeric"],
+        "group": "population"
+      },
+      "osm:education:operate_as": {
+        "validation": ["validate_allowed_list"],
+        "options": {"allowed_list": ["day_school","day_and_boarding","boarding"]},
+        "group": "basic"
+      },
+      "osm:education:pta": {
+        "validation": ["validate_yes_no"],
+        "group":"basic"
+      },
+      "osm:education:program_counselling": {
+        "validation": ["validate_yes_no"],
+        "group": "services"
+      },
+      "osm:education:program_feeding": {
+        "validation": ["validate_yes_no"],
+        "group": "services"
+      },
+      "osm:education:program_feeding_description": {
+        "validation": [],
+        "group": "services"
+      },
+      "osm:education:program_orphans": {
+        "validation": ["validate_yes_no"],
+        "group": "services"
+      },
+      "osm:education:program_sanitary_towel": {
+        "validation": ["validate_yes_no"],
+        "group": "services"
+      },
+      "osm:education:program_special_needs": {
+        "validation": ["validate_yes_no"],
+        "group": "services"
+      },
+      "osm:education:school_head": {
+        "validation": ["validate_present"],
+        "group": "basic"
+      },
+      "osm:education:students": {
+        "validation": ["validate_numeric"],
+        "group": "population"
+      },
+      "osm:education:students_female": {
+        "validation": ["validate_numeric"],
+        "group": "population"
+      },
+      "osm:education:students_male": {
+        "validation": ["validate_numeric"],
+        "group": "population"
+      },
+      "osm:education:teachers": {
+        "validation": ["validate_numeric"],
+        "group": "population"
+      },
+      "osm:education:teachers_female": {
+        "validation": ["validate_numeric"],
+        "group": "population"
+      },
+      "osm:education:teachers_male": {
+        "validation": ["validate_numeric"],
+        "group": "population"
+      },
+      "osm:education:teachers_trained": {
+        "validation": ["validate_numeric"],
+        "group": "population"
+      },
+      "osm:education:tsc_employed": {
+        "validation": ["validate_yes_no"],
+        "group": "population"
+      },
+      "osm:education:tsc_how_many": {
+        "validation": ["validate_numeric"],
+        "group": "population"
+      },
+      "osm:education:type": {
+        "validation": ["validate_allowed_list"],
+        "options": {"allowed_list": ["pre_primary","primary","secondary","vocational","library","other"]},
+        "group": "basic"
+      },
+      "osm:education:type_of_vocational_school": {
+        "validation": ["validate_allowed_list"],
+        "options": {"allowed_list": ["driving_school","hair_dressing","other"]},
+        "group": "basic"
+      },
+      "osm:electricity:operational_status": {
+        "validation": ["validate_allowed_list"],
+        "options": {"allowed_list": ["always","often","sometimes","never"]},
+        "group": "infrastructure"
+      },
+      "osm:image:classroom": {
+        "validation": ["validate_url"],
+        "group": "basic"
+      },
+      "osm:image:compound": {
+        "validation": ["validate_url"],
+        "group": "basic"
+      },
+      "osm:image:outside": {
+        "validation": ["validate_url"],
+        "group": "basic"
+      },
+      "osm:operator:description": {
+        "validation": ["validate_present"],
+        "group": "basic"
+      },
+      "osm:operator:type": {
+        "validation": ["validate_allowed_list"],
+        "options": {"allowed_list": ["cbo","government","ngo","ngo_international","private","religious"]},
+        "group": "basic"
+      },
+      "osm:toilet:arranged_name": {
+        "validation": ["validate_present"],
+        "group": "infrastructure"
+      },
+      "osm:toilets:male": {
+        "validation": ["validate_numeric"],
+        "group": "infrastructure"
+      },
+      "osm:toilets:female": {
+        "validation": ["validate_numeric"],
+        "group": "infrastructure"
+      },
+      "osm:toilet:present": {
+        "validation": ["validate_allowed_list"],
+        "options": {"allowed_list": ["yes","no","outside_public","arranged"]},
+        "group": "infrastructure"
+      },
+      "osm:toilets:teacher": {
+        "validation": ["validate_numeric"],
+        "group": "infrastructure"
+      },
+      "osm:toilets:handwashing": {
+        "validation": ["validate_yes_no"],
+        "group": "infrastructure"
+      },
+      "osm:water:source": {
+        "validation": ["validate_allowed_list"],
+        "options": {"allowed_list": ["tap","water_tank"]},
+        "group": "infrastructure"
       },
       "_unallowed": {
         "validation" : ["validate_unallowed"],
-        "filter": ["filter_unallowed"]
+        "filter": ["filter_unallowed"],
+        "group" : "basic"
       }
 /*
       "osm:education:type" : {
@@ -403,5 +718,5 @@ $( function() {
 
     }
   );
-  target.load_geojson("osm.geojson");
+  target.load_geojson("kangemi-schools-osm.geojson");
 });
